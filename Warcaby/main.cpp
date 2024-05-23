fae89dbf34332fd62ce4bf1928d1be5c0031b6ce
WiktorLewicki <94800385+WiktorLewicki@users.noreply.github.com>
2024-05-23 17:55:10 +0200

    Update main.cpp

--- Warcaby/main.cpp
+++ Warcaby/main.cpp
@@ -13,20 +13,22 @@
 #include <bits/stdc++.h>
 #include "bot.cpp"
 using namespace std;
+place gra;
+bool ruch(){
+	int pm=0;
+	do{
+		pm=gra.ruch_gracza();
+	}while(gra.czyja_tura()==0&&pm!=-1);
+	if(pm==-1) return false;
+	do{
+		ans ruch=bot(gra);
+		pm=gra.ruch_gracza(true, ruch.a, ruch.b, ruch.c, ruch.d, true);
+	}while(gra.czyja_tura()==1&&pm!=-1);
+	if(pm==-1) return false;
+	return true;
+}
 int main(){
-	place plansza;
-	plansza.init();
-	while(true){
-		int pm;
-		do{
-			pm=plansza.ruch_gracza();
-		}while(plansza.czyja_tura()==0&&pm!=-1);
-		if(pm==-1) break;
-		do{
-			ans ruch=bot(plansza);
-			pm=plansza.ruch_gracza(true, ruch.a, ruch.b, ruch.c, ruch.d, true);
-		}while(plansza.czyja_tura()==1&&pm!=-1);
-		if(pm==-1) break;
-	}
+	gra.init();
+	while(ruch());
 	return 0;
 }
